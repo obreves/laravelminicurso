30512d0c87db08287bb298a36dcda5e9d3a90a27
android <android@LABFAE.local>
2024-10-22 22:38:10 -0300

    laravelprimeiro

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\CategoriaController;
+use App\Models\Categoria;
+use Illuminate\Support\Facades\Route;
+
+    Route::prefix('/categorias')->group(function (){
+        Route::post('/', [CategoriaController::class, 'store']);
+        Route::get('/', [CategoriaController::class, 'index']);
+        Route::patch('/{id}' , [CategoriaController::class, 'update']);
+        Route::delete('/{id}', [CategoriaController::class, 'destroy']);
+        Route::get('/{id}', [CategoriaController::class, 'show']);
+
+    });
+?>
